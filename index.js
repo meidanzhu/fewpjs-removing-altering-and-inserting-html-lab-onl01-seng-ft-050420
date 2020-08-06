f46e7a22436cd213ae65f6a89801ff38fd80809a
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-08-06 00:17:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
  let main = document.querySelector('main')
  main.remove();
+
+let newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = 'YOUR-NAME is the champion'
+document.head.appendChild('newHeader')
